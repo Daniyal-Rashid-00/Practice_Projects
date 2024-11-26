00fde8572eaab0a75e35d74b9c8f57e1e99bc92b
Daniyal <everything4u1212@gmail.com>
2024-11-26 17:50:21 +0500

    Update

--- Practice.py
+++
@@ -1,9 +0,0 @@
-import requests
-import tkinter as tk
-from tkinter import messagebox
-
-# Function to get the weather
-def get_weather():
-    city = city_get.get()
-    api_key = "fa861ae77e724cef99c51659241411"
-    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
